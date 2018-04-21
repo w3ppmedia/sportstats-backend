a8a1708c62504cb4ff1b3038f32ac3be2fd6b52a
cyril-jumo <cyril.nxumalo@jumo.world>
2018-04-21 13:10:41 +0200

    starting resque

--- config/puma.rb
+++ config/puma.rb
@@ -23,6 +23,12 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
+on_worker_boot do
+  if defined?(Resque)
+    Resque.redis = ENV["REDIS_URL"] || "redis://127.0.0.1:6379"
+  end
+end
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
